6f750de3d010ddf308b2c9ff07ec06ae915f72f7
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2016-01-11 14:34:33 -0600

    Added url holder.

---
+++ app/src/main/java/zerobase/us/lunchbox/utils/UrlHolder.java
@@ -0,0 +1,12 @@
+package zerobase.us.lunchbox.utils;
+
+/**
+ * This is the class that holds the URL.
+ * Created by Sripad on 1/11/2016.
+ */
+public class UrlHolder {
+
+    public static String BASE_URL() {
+        return "https://sheetsu.com/apis/v1.0";
+    }
+}
