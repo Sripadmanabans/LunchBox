94d3b98a89ea22730fdb9b3409359c51fd947330
Sripadmanaban Subramanian <sripadmanabans@gmail.com>
2016-01-11 18:45:30 -0600

    Added the activity

---
+++ app/src/main/java/zerobase/us/lunchbox/activities/ShopActivity.java
@@ -0,0 +1,34 @@
+package zerobase.us.lunchbox.activities;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import zerobase.us.lunchbox.R;
+import zerobase.us.lunchbox.base.BaseActivity;
+
+/**
+ * This is the activity that is used to open and close the truck shop.
+ * Created by Sripad on 1/11/2016.
+ */
+public class ShopActivity extends BaseActivity {
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.collapsing_toolbar)
+    CollapsingToolbarLayout collapsingToolbarLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbarLayout.setTitle(getString(R.string.app_name));
+    }
+}
